657c95890331938cfea384e432f18b70a5ebc622
Juarez Martins <juniorbmx13@gmail.com>
2023-10-06 01:40:09 -0300

    "feat: inserção usuário - Método POST"

--- src/main/java/com/jucadev/workshopmongo/service/UserService.java
+++ src/main/java/com/jucadev/workshopmongo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.jucadev.workshopmongo.service;
 
 import com.jucadev.workshopmongo.domain.User;
+import com.jucadev.workshopmongo.dto.UserDTO;
 import com.jucadev.workshopmongo.repository.UserRepository;
 import com.jucadev.workshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = userRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
