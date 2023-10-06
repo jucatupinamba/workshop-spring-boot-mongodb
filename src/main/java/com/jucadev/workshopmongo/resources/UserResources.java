9c504b527651ad332db8044af48ad806689ce09b
Juarez Martins <juniorbmx13@gmail.com>
2023-10-06 09:30:52 -0300

    "feat: atualização de usuário - Método PUT"

--- src/main/java/com/jucadev/workshopmongo/resources/UserResources.java
+++ src/main/java/com/jucadev/workshopmongo/resources/UserResources.java
@@ -41,9 +41,17 @@ public class UserResources {
         return ResponseEntity.created(uri).build();
     }
 
-    @RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> delete(@PathVariable String id){
         userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id){
+        User obj = userService.fromDTO(objDTO);
+        obj.setId(id);
+        userService.update(obj);
         return ResponseEntity.noContent().build();
     }
 }
