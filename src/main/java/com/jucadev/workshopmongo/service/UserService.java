7b4037c45d00e248b24616da9b28681d274f9885
Juarez Martins <juniorbmx13@gmail.com>
2023-10-06 00:48:49 -0300

    "feat: implements findById; tratamento de exceção(correção http request)"

--- src/main/java/com/jucadev/workshopmongo/service/UserService.java
+++ src/main/java/com/jucadev/workshopmongo/service/UserService.java
@@ -2,10 +2,12 @@ package com.jucadev.workshopmongo.service;
 
 import com.jucadev.workshopmongo.domain.User;
 import com.jucadev.workshopmongo.repository.UserRepository;
+import com.jucadev.workshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +16,10 @@ public class UserService {
     private UserRepository userRepository;
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
