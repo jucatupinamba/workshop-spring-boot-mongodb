ba7c9af8406cb771aff44891297bdfe2026b626d
Juarez Martins <juniorbmx13@gmail.com>
2023-10-05 21:59:50 -0300

    "feat: implements UserDTO  refactor: User Resources"

--- src/main/java/com/jucadev/workshopmongo/resources/UserResources.java
+++ src/main/java/com/jucadev/workshopmongo/resources/UserResources.java
@@ -1,6 +1,7 @@
 package com.jucadev.workshopmongo.resources;
 
 import com.jucadev.workshopmongo.domain.User;
+import com.jucadev.workshopmongo.dto.UserDTO;
 import com.jucadev.workshopmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -19,8 +21,9 @@ public class UserResources {
     @Autowired
     private UserService userService;
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = userService.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
