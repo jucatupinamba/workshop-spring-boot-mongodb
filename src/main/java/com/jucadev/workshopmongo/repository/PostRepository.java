c5e6006c8e042570c3f66f9eb086e8114c38ed91
Juarez Martins <juniorbmx13@gmail.com>
2023-10-18 01:14:49 -0300

    "feat: consulta simples pelo título com @Query"

--- src/main/java/com/jucadev/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/jucadev/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.jucadev.workshopmongo.repository;
 import com.jucadev.workshopmongo.domain.Post;
 import com.jucadev.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ title: { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
